00aaf84449deda6891350ebe864d0f241f21724e
Danilo da Silva Santos <danilo.santos@saebti.intranet>
2019-12-12 17:46:18 -0300

    Implementa relatório de execução

--- patic/reports2/views.py
+++ patic/reports2/views.py
@@ -20,33 +20,38 @@ def report2(request):
     canv = Canvas(buffer, pagesize=landscape(letter))
 
     filename = filepath().split("/")[-1].split("\\")[-1].split(".")[0]
-    orgao = filename.replace('PA ', '').replace('  ', ' ').replace(' 2020 (ultimo)', '').replace(' 2020', '')
+    orgao = filename.replace('  ', ' ').replace('Execução PA ', '').replace('Execucao PA ', '').replace(' 2019 (ultimo)', '').replace(' 2019', '')
     df = dadospa()
 
     for index, row in df.iterrows():
 
+        contratada = row[8] == "Contratada/Adquirida"
+        cancelada = (row[8] == "Cancelada") or (row[8] == "Suspensa")
         nomerelatorio = Paragraph("<bold><font size=20 color='white'>Relatório de Execução</font></bold>", style)
         nomeorgao = Paragraph("<bold><font size=25 color='white'>{}</font></bold>".format(orgao), style)
         ano = Paragraph("<bold><font size=25 color='white'>2019</font></bold>", style)
 
         acao = Paragraph('<font size=12><b>Ação:</b><br /><br />{}</font>'.format(row[0]), style)
-        justificativa = Paragraph('<font size=12><b>Justificativa:</b><br /><br />{}</font>'.format(row[1]), styles['Normal'])
-        especificacao = Paragraph('<font size=12><b>Especificação do item:</b><br /><br />{}</font>'.format(row[2]), styles['Normal'])
-        objeto = Paragraph('<font size=12><b>Objeto de aquisição:</b><br /><br />{}</font>'.format(row[3]), styles['Normal'])
         contrato = Paragraph('<font size=12><b>{}</b></font>'.format(row[5]), style)
-        duracao = Paragraph('<font size=12><b>Duração do contrato:</b> {} meses</font>'.format(row[4]), style)
-        quantidade = Paragraph('<font size=12><b>Quantidade:</b> {}</font>'.format(row[6]), style)
-        valor = Paragraph('<font size=12><b>Valor:</b> R$ {}</font>'.format(moeda(row[7])), style)
+        quantidade = Paragraph('<font size=12><b>Quantidade Planejada:</b> {}</font>'.format(row[6]), style)
+        quantidade_exec = Paragraph('<font size=12><b>Quantidade Executada:</b> {}</font>'.format(row[9]), style) if contratada else ""
+        execucao_fisica = Paragraph('<font size=12><b>Execução Física:</b> {} %</font>'.format(round(row[9]/row[6]*100,0)), style) if contratada else ""
+        valor = Paragraph('<font size=12><b>Valor Planejado:</b> R$ {}</font>'.format(moeda(row[7])), style)
+        valor_exec = Paragraph('<font size=12><b>Valor Executado:</b> R$ {}</font>'.format(moeda(row[10])), style) if contratada else ""
+        execucao_financeira = Paragraph('<font size=12><b>Execução Financeira:</b> {} %</font>'.format(round(row[10]/row[7]*100,0)), style) if contratada else ""
+        status = Paragraph('<font size=12><b>Status da Ação:</b> {}</font>'.format(row[8]), styles['Normal'])
+        motivo = Paragraph('<font size=12><b>Motivo:</b> {}</font>'.format(row[11]), styles['Normal']) if cancelada else ""
 
         data = [
                 [acao, ''],
-                [justificativa, ''],
-                [especificacao, objeto],
-                [contrato, duracao],
-                [quantidade, valor]
+                [contrato, ''],
+                [quantidade, valor],
+                [quantidade_exec, valor_exec],
+                [execucao_fisica, execucao_financeira],
+                [status, motivo],
                ]
 
-        t = Table(data, colWidths=[420, 250], rowHeights=[70, 200, 80, 30, 30])
+        t = Table(data, colWidths=[335, 335], rowHeights=[70, 40, 40, 40, 40, 40])
         t.setStyle(TableStyle([("BOX", (0, 0), (-1, -1), 0.25, colors.black),
                                ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
                                ('SPAN', (0, 0), (1, 0)),
@@ -77,7 +82,7 @@ def report2(request):
         nomerelatorio.drawOn(canv, 72 + 26, 510)
         nomeorgao.drawOn(canv, 460, 540)
         ano.drawOn(canv, 660, 540)
-        t.drawOn(canv, 72, 70)
+        t.drawOn(canv, 72, 210)
         canv.drawString(400, 40, "Ação {}".format(index + 1))
         canv.showPage()
 
