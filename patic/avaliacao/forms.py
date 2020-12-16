71838dfe87d0040869136745e8a066d0db8ec29c
dssantos <danilosantos1986@gmail.com>
2020-12-16 16:52:31 -0300

    Implementa gerador de ficha de avaliação

--- patic/avaliacao/forms.py
+++ patic/avaliacao/forms.py
@@ -3,6 +3,17 @@ from django import forms
 
 class AvaliacaoForm(forms.Form):
     planilha_de_acao = forms.FileField()
+    doc_rel_acoes = forms.CharField(max_length=100)
+    orgao = forms.CharField(max_length=100)
+    avaliador1 = forms.CharField(max_length=100, required=False)
+    orgao_avaliador1 = forms.CharField(max_length=100, required=False)
+    avaliador2 = forms.CharField(max_length=100, required=False)
+    orgao_avaliador2 = forms.CharField(max_length=100, required=False)
+    avaliador3 = forms.CharField(max_length=100, required=False)
+    orgao_avaliador3 = forms.CharField(max_length=100, required=False)
+    representante_sgi = forms.CharField(max_length=100, required=False)
+    representante_prodeb = forms.CharField(max_length=100, required=False)
+    consideracoes_avaliador = forms.CharField(max_length=500, widget=forms.Textarea(attrs={'cols': 60, 'rows': 3}), required=False)
 
 
 class PrintAvaliacaoForm(forms.Form):
--- patic/avaliacao/views.py
+++ patic/avaliacao/views.py
@@ -10,10 +10,22 @@ def avaliacao(request):
         form = AvaliacaoForm(request.POST, request.FILES)
         if form.is_valid():
             context = ficha_avaliacao_context(request.FILES['planilha_de_acao'])
+            context['orgao'] = form.cleaned_data['orgao']
+            context['doc_rel_acoes'] = form.cleaned_data['doc_rel_acoes']
+            context['avaliador1'] = form.cleaned_data['avaliador1']
+            context['orgao_avaliador1'] = form.cleaned_data['orgao_avaliador1']
+            context['avaliador2'] = form.cleaned_data['avaliador2']
+            context['orgao_avaliador2'] = form.cleaned_data['orgao_avaliador2']
+            context['representante_sgi'] = form.cleaned_data['representante_sgi']
+            context['representante_prodeb'] = form.cleaned_data['representante_prodeb']
+            context['consideracoes_avaliador'] = form.cleaned_data['consideracoes_avaliador']
             PrintAvaliacaoFormSet = formset_factory(PrintAvaliacaoForm, extra=len(context['acoes']))
             formset = PrintAvaliacaoFormSet()
             for form, acao in zip(formset, context['acoes']):
                 acao['indicacao'] = form
+            
+            
+
             return render(request, 'avaliacao/ficha_avaliacao.html', context=context)
 
     if request.method == 'POST' and 'btn_print' in request.POST:
