f7e839a415e6ae56e88a8695974b4ced66eeb71e
dssantos <danilosantos1986@gmail.com>
2021-11-17 14:49:32 -0300

    Ajusta ficha de avaliação para o novo formato de planilha de ação

--- patic/avaliacao/models.py
+++ patic/avaliacao/models.py
@@ -4,6 +4,7 @@ import pandas as pd
 from patic.core.models import moeda
 
 def ficha_avaliacao_context(docfile):
+    print(type(docfile))
     
     nome = docfile.name[:-5]
     frame = raw_dataset(docfile)    
@@ -19,12 +20,13 @@ def ficha_avaliacao_context(docfile):
     return {'acoes': acoes, 'nome':nome}
 
 def raw_dataset(file):
+    print(type(file))
     
     df = pd.ExcelFile(file)
     df = df.parse("Planejamento")
     df = df.iloc[7:]
-    df = df.iloc[:, :9]
-    df.columns = ['unidade', 'acao', 'justificativa', 'especificacao_do_item', 'objeto', 
+    df = df.iloc[:, :10]
+    df.columns = ['id', 'unidade', 'acao', 'justificativa', 'especificacao_do_item', 'objeto', 
                   'duracao', 'contrato_novo_ou_renovacao', 'quantidade', 'valor']
     df = df.dropna(subset=['acao'])
     df = df.where(pd.notnull(df), None)
