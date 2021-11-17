949464bb1464978be4c7b328b863425888c25832
dssantos <danilosantos1986@gmail.com>
2021-11-17 11:57:47 -0300

    Ajusta relatório para aceitar novo formato de planilha de execução

--- patic/reports2/models.py
+++ patic/reports2/models.py
@@ -8,9 +8,7 @@ from patic.core.models import filepath
 def dadospa():
     xl = pd.ExcelFile(filepath('Execução'))
     df = xl.parse("Execução")
-    df = df.drop(columns=['Unnamed: 16', 'Unnamed: 17', 'Unnamed: 18', 'Unnamed: 19', 'Unnamed: 20', 'Unnamed: 21', 'Unnamed: 22'])
-    df = df.iloc[6:]
-    df = df.dropna(subset=['Unnamed: 2'])
+    df = df.dropna(subset=['Unnamed: 1'])
     df = df.drop(df.index[0])
     df = df.reset_index(drop=True)
     return df
