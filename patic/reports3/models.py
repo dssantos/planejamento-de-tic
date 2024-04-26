c355cd39517390aec0df40a86702b1557d8487e5
Danilo Santos <danilosantos1986@gmail.com>
2024-04-26 16:16:37 -0300

    Implements file upload to generate executions report

--- patic/reports3/models.py
+++ patic/reports3/models.py
@@ -12,3 +12,11 @@ def dadospa():
     df = df.drop(df.index[0])
     df = df.reset_index(drop=True)
     return df
+
+def file_acoes_to_df(file):
+    xl = pd.ExcelFile(file)
+    df = xl.parse("Execução")
+    df = df.dropna(subset=['Unnamed: 1'])
+    df = df.drop(df.index[0])
+    df = df.reset_index(drop=True)
+    return df
